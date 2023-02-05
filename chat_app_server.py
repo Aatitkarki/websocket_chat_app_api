04be38ac8bd82db1578ac23cf2c2e5b4ddeec014
Manishkarke <mk4345437@gmail.com>
2023-02-05 06:13:40 +0545

    chat app websocket server created

---
+++ chat_app_server.py
@@ -0,0 +1,50 @@
+from simple_websocket_server import WebSocketServer, WebSocket
+
+# Change to your IP
+hostname="192.168.1.64"
+# Data Structure
+# { address: (uniqueId,name)}
+clients = {}
+
+class ChatServer(WebSocket):
+    def connected(self):
+        print(self.address, 'connected')
+
+    def handle_close(self):
+        print(self.address, 'closed')
+
+    def handle(self):
+        userId = None
+        try:
+            message = self.data
+            if message is None:
+                return
+            elif message.startswith("register:"):
+                userId, userName = message.split(":")[1].split(",")
+                for key,value in clients.items():
+                    if(key != self.address[0]):
+                        value[2].send_message(f"active_user:{userId},{userName}")
+                clients[self.address[0]] = (userId, userName,self)
+            elif message.startswith("unregister:"):
+                userId = message.split(":")[1]
+                clients.pop(self.address, None)
+                for key,value in clients.items():
+                    self.send_message(f"inactive_user:{value[0]},{value[1]}")
+            elif message.startswith("message:"):
+                recipientId,senderId,message = message.split(":")[1].split(",")
+                # recipient = clients.get(recipientId)
+                for key,value in clients.items():
+                    # if(value[0] == recipientId):
+                    print("Sending message")
+                    value[2].send_message(f"senderId:{senderId},message:{message}")
+
+            elif message == "list users":
+                for key,value in clients.items():
+                    if(key != self.address[0]):
+                        print("Seding User fom list")
+                        self.send_message(f"active_user:{value[0]},{value[1]}")
+        except Exception as e:
+            print(f"Error is: {e}")
+
+server = WebSocketServer(hostname,8000, ChatServer)
+server.serve_forever()
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Installation
+
+pip install simple-websocket-server
+
+
+# Todo
+Change hostname to your actual ip or change to 'localhost'
+
+# Run Server
+python3 chat_app_server.py
