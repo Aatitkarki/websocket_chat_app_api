638e077ee63ec71c61aec713efced19f68e98556
Suresh Karki <aatitkarki123@gmail.com>
2023-02-08 14:42:13 +0545

    fix: changed logic for clients

--- chat_app_server.py
+++ chat_app_server.py
@@ -1,7 +1,8 @@
 from simple_websocket_server import WebSocketServer, WebSocket
 
 # Change to your IP
-hostname="192.168.1.64"
+hostname="10.0.0.78"
+# hostname="192.168.1.64"
 # Data Structure
 # { address: (uniqueId,name)}
 clients = {}
@@ -17,6 +18,7 @@ class ChatServer(WebSocket):
         userId = None
         try:
             message = self.data
+            print("There is something")
             if message is None:
                 return
             elif message.startswith("register:"):
@@ -24,7 +26,7 @@ class ChatServer(WebSocket):
                 for key,value in clients.items():
                     if(key != self.address[0]):
                         value[2].send_message(f"active_user:{userId},{userName}")
-                clients[self.address[0]] = (userId, userName,self)
+                clients[self.address[0]+userId] = (userId, userName,self)
             elif message.startswith("unregister:"):
                 userId = message.split(":")[1]
                 clients.pop(self.address, None)
@@ -38,13 +40,16 @@ class ChatServer(WebSocket):
                     print("Sending message")
                     value[2].send_message(f"senderId:{senderId},message:{message}")
 
-            elif message == "list users":
+            elif message.startswith("list users"):
+                print("listing users")
+                userId = message.split(":")[1]
+                print(f"Listing users {userId} {message}")
                 for key,value in clients.items():
-                    if(key != self.address[0]):
+                    if(key != self.address[0+userId]):
                         print("Seding User fom list")
                         self.send_message(f"active_user:{value[0]},{value[1]}")
         except Exception as e:
             print(f"Error is: {e}")
-
+print("hlello world")
 server = WebSocketServer(hostname,8000, ChatServer)
 server.serve_forever()
